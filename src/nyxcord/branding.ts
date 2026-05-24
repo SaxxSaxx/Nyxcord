9844b169cbbcefae5ac2058de573739ff7d1f78a
saxx <79324095+SaxxSaxx@users.noreply.github.com>
2026-05-24 23:07:34 +0200

    add Nyxcord branding constants module

---
+++ src/nyxcord/branding.ts
@@ -0,0 +1,12 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+export const BRAND_NAME = "Nyxcord";
+export const BRAND_ACCENT = "#a855f7";
+export const BRAND_REPO_OWNER = "SaxxSaxx";
+export const BRAND_REPO_NAME = "Nyxcord";
+export const BRAND_REPO_URL = `https://github.com/${BRAND_REPO_OWNER}/${BRAND_REPO_NAME}`;
+export const BRAND_UPDATE_URL = `${BRAND_REPO_URL}.git`;
